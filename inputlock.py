d920464081e9bab0619c44fd5e30e0c70672968b
SineshX <48027382+SineshX@users.noreply.github.com>
2021-07-12 19:04:08 +0530

    Update inputlock.py
    updated

--- inputlock.py
+++ inputlock.py
@@ -6,36 +6,31 @@ import threading
 
 def blockinput_start():
         mouse = Controller()
-        # global block_input_flag
+        global block_input_flag
         for i in range(150):
             keyboard.block_key(i)
         while block_input_flag == 1:
-            mouse.position = (0, 0) #lock mouse position 
+            mouse.position = (0, 0)
     
-def block_input_stop():
-        # global block_input_flag
-        block_input_flag = 0 # changed position
-        # means mouse cursor sahi ho jayega 
+    
+def unblockinput():
+    blockinput_stop()
+    print("[SUCCESS] Input unblocked!")
+    
+
+def blockinput_stop():
+        global block_input_flag
         for i in range(150):
             keyboard.unblock_key(i)
+        block_input_flag = 0
+
 
 def blockinput():
-    global block_input_flag #first  time declaration 
-    block_input_flag = 1   
-    # init
-    t1 = threading.Thread(target=blockinput_start())
+    global block_input_flag
+    block_input_flag = 1
+    t1 = threading.Thread(target=blockinput_start)
     t1.start()
     print("[SUCCESS] Input blocked!")
-    # todo : use colorma to print on big screen
-    
-
-def unblockinput():
-    block_input_stop()
-    print("[SUCCESS] Input unblocked!")
-    # todo : use colorma to print on big screen
-
-    
-
     
 
 
